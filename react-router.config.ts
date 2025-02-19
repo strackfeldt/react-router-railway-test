6f8016cb87ce3495f81da57b7fe7c5fe1acb55a3
Timo Strackfeldt <timo@strackfeldt.com>
2025-02-19 07:42:51 +0100

    wip

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,4 +2,9 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
+  future: {
+    unstable_optimizeDeps: true,
+    unstable_viteEnvironmentApi: true,
+    unstable_splitRouteModules: true,
+  },
 } satisfies Config;
